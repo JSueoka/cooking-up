2b2d201934e5df3d092f02431c5249c86f52e068
Jhonattan Sueoka <jhonattansueoka@gmail.com>
2025-05-20 11:51:03 -0300

    Primeiro commit. Subindo projeto final

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script lang="ts">
+import Banner from "./components/Banner.vue";
+import ConteudoPrincipal from "./components/ConteudoPrincipal.vue";
+import Rodape from "./components/Rodape.vue";
+
+  
+export default {
+  components: {Banner, ConteudoPrincipal, Rodape}
+  // Isso foi feito pois o js permite deixar apenas o valor quando valor = componente
+  // components: { Banner: Banner }
+}
+</script>
+
+<template>
+  <Banner />
+  <ConteudoPrincipal />
+  <Rodape />
+</template>
---
+++ src/components/SuaLista.vue
@@ -0,0 +1,60 @@
+<script lang="ts">
+import type { PropType } from "vue";
+import Tag from "./Tag.vue";
+
+export default {
+  props: {
+    ingredientes: { type: Array as PropType<string[]>, required: true },
+  },
+  components: { Tag },
+};
+</script>
+
+<template>
+  <section>
+    <span class="subtitulo-lg sua-lista-texto"> Sua lista: </span>
+
+    <ul v-if="ingredientes.length" class="ingredientes-sua-lista">
+      <!-- lembrando que posso trocar v-bind: por apenas :key -->
+      <li v-for="ingrediente in ingredientes" v-bind:key="ingrediente">
+        <!-- poderia usar somente ativa, sem o v-bind, mas assim fica mais claro -->
+        <Tag :texto="ingrediente" :ativa="true" />
+      </li>
+    </ul>
+
+    <p v-else class="paragrafo lista-vazia">
+      <img
+        src="../assets/imagens-main/images/icones/lista-vazia.svg"
+        alt="Ícone de pesquisa"
+      />
+      Sua lista está vazia, selecione ingredientes para iniciar.
+    </p>
+  </section>
+</template>
+
+<style scoped>
+.sua-lista-texto {
+  color: var(--coral, #f0633c);
+  display: block;
+  text-align: center;
+  margin-bottom: 1.5rem;
+}
+
+.ingredientes-sua-lista {
+  display: flex;
+  justify-content: center;
+  gap: 1rem 1.5rem;
+  flex-wrap: wrap;
+}
+
+.lista-vazia {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-wrap: wrap;
+  gap: 0.25rem;
+
+  color: var(--coral, #f0633c);
+  text-align: center;
+}
+</style>
